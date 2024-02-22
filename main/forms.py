bbb268d707a936071a07d5fed48cb63e84a831cc
David Mojica <davidmojicav@gmail.com>
2024-02-22 16:30:29 -0500

    Bool input set

--- main/forms.py
+++ main/forms.py
@@ -6,14 +6,23 @@ class registroUsuariosForm(forms.Form):
         label="Nombre",
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese el nombre del empleado/administrador'})
     )
+    
     documento=forms.CharField(
         label='Documento',
         widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Documento del usuario'})
     )
+    
     password = forms.CharField(
         label="Contraseña",
         widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Nueva contraseña'})
     )
+    
+    usarDocumentoComoPassword = forms.BooleanField(
+        required=False,
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        label="Usar documento como contraseña"
+    )
+    
     tipo_usuario=forms.ModelChoiceField(
         label="Tipo de usuario",
         queryset=TipoUsuario.objects.all(),
@@ -23,5 +32,5 @@ class registroUsuariosForm(forms.Form):
     
     class Meta:
         model = Usuarios
-        fields = ('nombre', 'documento', 'password', 'tipo_usuario')
+        fields = ('nombre', 'documento', 'password', 'usarDocumentoComoPassword', 'tipo_usuario')
     
