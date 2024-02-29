a810407305aae288b1f0aa76e33130ce60584cc7
David Mojica <davidmojicav@gmail.com>
2024-02-29 18:09:24 -0500

    ETL - Transform into csv

--- extras/productosETL/csvConverter.py
+++ extras/productosETL/csvConverter.py
@@ -1,7 +1,15 @@
 import pandas as pd
 
-ruta_archivo_xlsx = "unificado_sin_cabecera.xlsx"
-ruta_archivo_csv = "unificado_final.csv"
+ruta_archivo_xlsx = "C:\\Users\\swan5\\Desktop\\universidad\\projects\\works\\Ducaplast\\extras\\productosETL\\prod.xlsx"
+ruta_archivo_csv = "C:\\Users\\swan5\\Desktop\\universidad\\projects\\works\\Ducaplast\\extras\\productosETL\\productos_final.csv"
 
-data_frame = pd.read_excel(ruta_archivo_xlsx)
-data_frame.to_csv(ruta_archivo_csv, sep='|', index=False)
+converters = {
+    "DESCRIPCIÓN": lambda x: x.replace("'", "").replace('"', ''),
+    "REFERENCIA DE FÁBRICA": lambda x: x.replace("'", "").replace('"', '')
+}
+data_frame = pd.read_excel(ruta_archivo_xlsx,converters=converters)
+
+data_frame = data_frame.applymap(lambda x: ' '.join(x.split()) if isinstance(x, str) else x)
+
+data_frame.to_csv(ruta_archivo_csv, sep='|', index=False, header=False)
+print("Hecho!")
