b1204d35b3b132f7d98c14b466c21c2f06c9f765
David Mojica <davidmojicav@gmail.com>
2024-03-08 11:05:41 -0500

    Total price

--- main/static/javascript/cart.js
+++ main/static/javascript/cart.js
@@ -4,50 +4,10 @@ const createClient = document.getElementById('createClient');
 const openCreateClient = document.getElementById('openCreateClient');
 const openSelectClient = document.getElementById('openSelectClient');
 
+const total_productos = document.getElementById('total_productos');
+let totalPrice = 0;
 
-document.querySelectorAll('.product_quantity').forEach(input => {
-    const priceElement = input.parentElement.nextElementSibling.querySelector('span');
-    const price = parseFloat(priceElement.textContent.replace('$', '').replace('.', ''));
-
-    input.addEventListener('input', function() {
-        const quantity = parseInt(this.value);
-        const total = quantity * price;
-        priceElement.textContent = addDecimal(total);
-    });
-
-    const stepDown = input.parentElement.querySelector('button:first-child');
-    const stepUp = input.parentElement.querySelector('button:last-child');
-
-    stepDown.addEventListener('click', e =>{
-        input.stepDown();
-        const quantity = parseInt(input.value);
-        const total = quantity * price;
-        priceElement.textContent = addDecimal(total);
-    });
-    stepUp.addEventListener('click', e =>{
-        input.stepUp();
-        const quantity = parseInt(input.value);
-        const total = quantity * price;
-        priceElement.textContent = addDecimal(total);
-    });
-});
-
-
-function addDecimal(number) {
-    const str = number.toFixed(2);
-    const parts = str.split('.');
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return parts.join('.');
-}
-
-
-
-
-
-
-
-
-
+//Mostrar detalles de cliente o crear cliente
 const showElement = element => {
     element.classList.remove('d-none');
     element.classList.add('d-block');
@@ -67,3 +27,47 @@ openSelectClient.addEventListener('click', (e) => {
     showElement(selectClient);
     hideElement(createClient);
 });
+
+//Actualizar el precio de cada producto
+document.querySelectorAll('.product_quantity').forEach(input => {
+    const priceElement = input.parentElement.nextElementSibling.querySelector('span');
+    const price = (parseFloat(priceElement.textContent.replace('$', '').replace('.', '')))/input.value;
+    totalPrice += price*input.value;
+    console.log(totalPrice)
+    const stepDown = input.parentElement.querySelector('button:first-child');
+    const stepUp = input.parentElement.querySelector('button:last-child');
+
+    input.addEventListener('input', function() {
+        updateProductPrice(priceElement, price, parseInt(this.value));
+    });
+
+    stepDown.addEventListener('click', e =>{
+        input.stepDown();
+        updateProductPrice(priceElement, price, parseInt(input.value));;
+    });
+    stepUp.addEventListener('click', e =>{
+        input.stepUp();
+        updateProductPrice(priceElement, price, parseInt(input.value));
+    });
+});
+
+const updateGlobalPrice = () => {
+    total_productos.textContent = addDecimal(totalPrice);
+}
+
+const updateProductPrice = (element, price, quantity) => {
+    if (!isNaN(quantity) && quantity !== 0){
+        const totalProductPrice = price * quantity;
+        element.textContent = addDecimal(totalProductPrice);
+        totalPrice += totalProductPrice;
+        updateGlobalPrice();
+    } else {
+        productPriceElement.textContent = "Cantidad no válida, el producto se borrará si se confirma la venta.";
+    }
+}
+
+const addDecimal = n => {
+    const p = Math.trunc(n).toString().split('.');
+    p[0] = p[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return p.join('.');
+}
